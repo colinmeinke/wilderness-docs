83ce47db165c638721fd959289c9a49b50012676
Colin Meinke <hello@colinmeinke.com>
2017-09-10 14:41:30 -0500

    add motionPath api documentation

--- pages/api/the-motionpath-function.js
+++ pages/api/the-motionpath-function.js
@@ -2,6 +2,7 @@ import ButtonGroup from '../../components/button-group'
 import Code from '../../components/code'
 import Layout from '../../components/layout'
 import Link from '../../components/link'
+import Subtitle from '../../components/subtitle'
 import Title from '../../components/title'
 
 export default () => (
@@ -11,7 +12,67 @@ export default () => (
     </Title>
 
     <p>
-      Documentation coming soon.
+      Creates a <Link href="/advanced-features/motion-path">motion path</Link>
+      {' '} <Link href="/advanced-features/force">Force</Link> when passed a
+      {' '} <Link href="/api/definitions#plain-shape-object">Plain Shape
+      Object</Link>.
+    </p>
+
+    <Code>{`
+motionPath(plainShapeObject)
+    `}</Code>
+
+    <Subtitle id="arguments">
+      Arguments
+    </Subtitle>
+
+    <p>
+      <strong>
+        <Code inline>plainShapeObject</Code>
+      </strong>
+    </p>
+
+    <p>
+      The Plain Shape Object creates a motion path relative to the Shape it
+      is applied to. For this reason it should almost always start from the
+      point <Code inline>{`{ x: 0, y: 0 }`}</Code>.
+    </p>
+
+    <p>
+      This Plain Shape Object can take three properties that have special
+      significance in relation to a motion path.
+    </p>
+
+    <ul>
+      <li>
+        <Code inline>accuracy</Code> <small>(optional)</small> is a number, in
+        degrees, that Wilderness determines a curve "straight enough" to be
+        considered a straight line. This will determine how accurately a {' '}
+        <Link href="/api/definitions#shape">Shape</Link> follows a motion path.
+        However, the more accurate, the more calculations required, and
+        therefore the worse the performance.
+      </li>
+      <li>
+        <Code inline>easing</Code> <small>(optional)</small> is a string that
+        maps to a function defined in {' '}
+        <Link href="https://github.com/chenglou/tween-functions">Tween
+        Functions</Link>, or a custom easing function. This is the easing with
+        which the Shape travels the motion path.
+      </li>
+      <li>
+        <Code inline>rotate</Code> <small>(optional)</small> is a boolean or a
+        number that determines if the Shape should rotate along with the
+        motion path. If set to a number, this is the angle to which the Shape
+        is offset against the motion path's current angle.
+      </li>
+    </ul>
+
+    <Subtitle id="return-value">
+      Return value
+    </Subtitle>
+
+    <p>
+      A Force function.
     </p>
 
     <ButtonGroup>
