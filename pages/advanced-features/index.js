623c464c15e4ed0d5dd2d6cda6bedb481a846f79
Colin Meinke <hello@colinmeinke.com>
2017-09-10 21:12:29 -0500

    add advanced features index page

--- pages/advanced-features/index.js
+++ pages/advanced-features/index.js
@@ -10,7 +10,18 @@ export default () => (
     </Title>
 
     <p>
-      Documentation coming soon
+      Now that we've <Link href="/getting-started">learned the basics</Link>
+      {' '} it's time for us to pack our bags, say our goodbyes and head out
+      deep into the Wilderness 🌵⛺️.
+    </p>
+
+    <p>
+      This next set of lessons will showcase just how powerful Wilderness can
+      be when creating complex SVG animations.
+    </p>
+
+    <p>
+      Let's go!
     </p>
 
     <ButtonGroup>
