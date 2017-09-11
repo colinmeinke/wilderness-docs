176ffb102b2169b7245f6793e80f11731feb3090
Colin Meinke <hello@colinmeinke.com>
2017-09-11 15:09:11 -0500

    document plainShapeObject function

--- pages/api/the-plainshapeobject-function.js
+++ pages/api/the-plainshapeobject-function.js
@@ -1,5 +1,7 @@
 import Code from '../../components/code'
 import Layout from '../../components/layout'
+import Link from '../../components/link'
+import Subtitle from '../../components/subtitle'
 import Title from '../../components/title'
 
 export default () => (
@@ -9,7 +11,35 @@ export default () => (
     </Title>
 
     <p>
-      Documentation coming soon.
+      Creates a <Link href="/api/definitions#plain-shape-object">Plain Shape
+      Object</Link> when passed a {' '}
+      <Link href="/api/definitions#shape">Shape</Link>.
+    </p>
+
+    <Code>{`
+plainShapeObject(shape)
+    `}</Code>
+
+    <Subtitle id="arguments">
+      Arguments
+    </Subtitle>
+
+    <p>
+      <strong>
+        <Code inline>shape</Code>
+      </strong>
+    </p>
+
+    <p>
+      A Shape.
+    </p>
+
+    <Subtitle id="return-value">
+      Return value
+    </Subtitle>
+
+    <p>
+      A Plain Shape Object.
     </p>
   </Layout>
 )
